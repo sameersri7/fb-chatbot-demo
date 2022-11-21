757f66e3801f3d50279a347bd7c916b153163b80
sameersri7 <118708921+sameersri7@users.noreply.github.com>
2022-11-21 10:51:11 +0545

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -656,9 +656,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "100035276075204",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "49%3AqPVNURvG1bSo4w%3A2%3A1669006855%3A-1%3A3699"
 }
 
 
